2ce6416274276375ffa9d330bc935275492a4768
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-04-21 14:57:10 +0300

    Added new main file + css style

---
+++ src/PixelManMain.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PixelManMain.css'
+export  const PixelManMain=(props)=>{
+    
+    
+    
+    return(
+        <div>
+       <div className="mainContainer"></div>
+        </div>);
+}
+
+
+export default PixelManMain;
