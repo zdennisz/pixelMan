c1d1b6d5ca910953cc832c097e79534e244c7950
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-05-09 01:36:51 +0300

    Fixed monster appearing off screen

--- src/MonsterCreator.js
+++ src/MonsterCreator.js
@@ -1,25 +1,31 @@
 import getMonster from './constants'
 const minDistanceFromEdge=32;
-const offsetFromBothSides=64;
+
 function createMonster(height, width) {
 
     //create position according to game screen
     var monsterPic = new Image(16, 16);
     monsterPic.src = getMonster(Math.floor(Math.random() * 30));
-    var posX = minDistanceFromEdge+Math.floor(Math.random() * (height-offsetFromBothSides)) + 1
-    var posY = minDistanceFromEdge+Math.floor(Math.random() * (width-offsetFromBothSides)) + 1
+    var posX=getRandomNumber(1,width-minDistanceFromEdge);
+    var posY=getRandomNumber(1,height-minDistanceFromEdge);
     var monster = {
         position: {
             x: posX,
             y: posY
         }
         ,
-        speed: 1,
+        speed: 1.5,
         monsterImage: monsterPic,
         radius:16
     }
 
     return monster;
+}
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
