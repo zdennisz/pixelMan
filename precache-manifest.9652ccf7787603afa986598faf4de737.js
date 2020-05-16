56612c793163b7543381cea52401fdb93a65dbf9
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-05-16 19:33:58 +0300

    Updates

---
+++ precache-manifest.9652ccf7787603afa986598faf4de737.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92f3364190125df03b3901ea035f153",
+    "url": "/pixelMan/index.html"
+  },
+  {
+    "revision": "05cd28c0eafdc2ae8e63",
+    "url": "/pixelMan/static/css/main.4fd548b8.chunk.css"
+  },
+  {
+    "revision": "ce26c95affee71e6c390",
+    "url": "/pixelMan/static/js/2.c06c497b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixelMan/static/js/2.c06c497b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05cd28c0eafdc2ae8e63",
+    "url": "/pixelMan/static/js/main.e3880faf.chunk.js"
+  },
+  {
+    "revision": "4d91c5d6aed83df5f43d",
+    "url": "/pixelMan/static/js/runtime-main.65120a50.js"
+  },
+  {
+    "revision": "1b99bf8ef738811dceac8750c685e560",
+    "url": "/pixelMan/static/media/001_monster.1b99bf8e.png"
+  },
+  {
+    "revision": "20bb5ccb0df6447afd428daa954c55e0",
+    "url": "/pixelMan/static/media/002_monster.20bb5ccb.png"
+  },
+  {
+    "revision": "8ca73977dc5d8c2bbef4e2ce02e040d0",
+    "url": "/pixelMan/static/media/003_monster.8ca73977.png"
+  },
+  {
+    "revision": "5c6a4861e99894326462d1eaa4073f7e",
+    "url": "/pixelMan/static/media/004_monster.5c6a4861.png"
+  },
+  {
+    "revision": "2a3548bf7ff73466e6d27a044e5f059d",
+    "url": "/pixelMan/static/media/005_monster.2a3548bf.png"
+  },
+  {
+    "revision": "d32a68fdec95ce93381e30ac0cad3e10",
+    "url": "/pixelMan/static/media/006_monster.d32a68fd.png"
+  },
+  {
+    "revision": "fc584497b58b00fe677537233baa276d",
+    "url": "/pixelMan/static/media/007_monster.fc584497.png"
+  },
+  {
+    "revision": "321d886ce47ae111fa70abc1f4b5373b",
+    "url": "/pixelMan/static/media/008_monster.321d886c.png"
+  },
+  {
+    "revision": "a6eb3173b63b27f93cb97a643ec2b948",
+    "url": "/pixelMan/static/media/009_monster.a6eb3173.png"
+  },
+  {
+    "revision": "46e49d83aa59bd0d965b0a2c775c6b50",
+    "url": "/pixelMan/static/media/010_monster.46e49d83.png"
+  },
+  {
+    "revision": "1ede2a391e9c746d10b8c105eddc3e8e",
+    "url": "/pixelMan/static/media/011_monster.1ede2a39.png"
+  },
+  {
+    "revision": "35ad9ed65290216ad267ff87c9284b7d",
+    "url": "/pixelMan/static/media/012_monster.35ad9ed6.png"
+  },
+  {
+    "revision": "30052ef85ca26c8db6934c4dd0299ca8",
+    "url": "/pixelMan/static/media/013_monster.30052ef8.png"
+  },
+  {
+    "revision": "12b77903ba16466a668f0a043e897801",
+    "url": "/pixelMan/static/media/014_monster.12b77903.png"
+  },
+  {
+    "revision": "9d78e90b5db32d6cb83f5973b6b7b778",
+    "url": "/pixelMan/static/media/015_monster.9d78e90b.png"
+  },
+  {
+    "revision": "3486096df903a34f42e6c50cc12df717",
+    "url": "/pixelMan/static/media/016_monster.3486096d.png"
+  },
+  {
+    "revision": "815e0c962ec51d41ba21359771e66987",
+    "url": "/pixelMan/static/media/017_monster.815e0c96.png"
+  },
+  {
+    "revision": "e7b5b1cc688e60b441bb1afd8f9f9a77",
+    "url": "/pixelMan/static/media/018_monster.e7b5b1cc.png"
+  },
+  {
+    "revision": "d6349c953417c55bb2bb146d79e7bbf5",
+    "url": "/pixelMan/static/media/019_monster.d6349c95.png"
+  },
+  {
+    "revision": "221a598d9918af03ac5163b6d26199e4",
+    "url": "/pixelMan/static/media/020_monster.221a598d.png"
+  },
+  {
+    "revision": "a28f1c8afd9b486231e1acc0a55c43fb",
+    "url": "/pixelMan/static/media/021_monster.a28f1c8a.png"
+  },
+  {
+    "revision": "7fd831ffdd95af037777dc77e7523357",
+    "url": "/pixelMan/static/media/022_monster.7fd831ff.png"
+  },
+  {
+    "revision": "816ee777449a97b00da10c063fbad543",
+    "url": "/pixelMan/static/media/023_monster.816ee777.png"
+  },
+  {
+    "revision": "bfecc2c7e870f8a2af5b743f35f0606e",
+    "url": "/pixelMan/static/media/024_monster.bfecc2c7.png"
+  },
+  {
+    "revision": "e0ce978b3a5298f000632a0b5e1856f3",
+    "url": "/pixelMan/static/media/025_monster.e0ce978b.png"
+  },
+  {
+    "revision": "5c364dcee6de300afdb0979baca1655a",
+    "url": "/pixelMan/static/media/026_monster.5c364dce.png"
+  },
+  {
+    "revision": "69cf53b6af6cd9435ca6854548138a23",
+    "url": "/pixelMan/static/media/027_monster.69cf53b6.png"
+  },
+  {
+    "revision": "3bf4425475b32fbc5eeeeb0ec1094f99",
+    "url": "/pixelMan/static/media/028_monster.3bf44254.png"
+  },
+  {
+    "revision": "40c0b914e080bca48a4de6db2bf0a0ef",
+    "url": "/pixelMan/static/media/029_monster.40c0b914.png"
+  },
+  {
+    "revision": "8090483907847eee2f69df1bcfa19e7d",
+    "url": "/pixelMan/static/media/030_monster.80904839.png"
+  },
+  {
+    "revision": "680af361ee346de629b06f65447c0504",
+    "url": "/pixelMan/static/media/soundTrack.680af361.mp3"
+  }
+]);
